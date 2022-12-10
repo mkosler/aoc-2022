94e6f462d18c86373f2f39fa23ea5a379e96f936
Michael Kosler <michaelkosler@gmail.com>
2022-12-10 15:34:44 -0800

    Cleaned up Day10

--- 2022/Day10.cs
+++ 2022/Day10.cs
@@ -5,18 +5,61 @@ using System.Text;
 namespace aoc.Year2022;
 
 public class Day10 : IDay {
-  private readonly Dictionary<string, int> _cycleLengths = new Dictionary<string, int> {
-    ["noop"] = 1,
-    ["addx"] = 2,
-  };
+  private class CRT {
+    private readonly bool[] _pixels;
+    private readonly int _width;
+
+    public CRT(int width, int height) => (_width, _pixels) = (width, new bool[width * height]);
+
+    public void Set(int i, bool b) => _pixels[i] = b;
+
+    public override string ToString() {
+      return string.Join("\n",
+        _pixels.Chunk(_width).Select(l => string.Join("", l.Select(x => x ? "#" : "."))));
+    }
+  }
+
+  private class Prog {
+    private readonly List<(string Instruction, int StartCycle)> _instructions
+      = new List<(string Instruction, int StartCycle)>();
+
+    public static readonly Dictionary<string, int> CycleLengths = new Dictionary<string, int> {
+      ["noop"] = 1,
+      ["addx"] = 2,
+    };
+
+    public int RunFor(int cycles) {
+      return _instructions
+        .Where(x => x.StartCycle <= cycles - 2)
+        .Aggregate(1, (acc, x) => acc + GetValue(x.Instruction));
+    }
+
+    public static Prog GenerateFromInput(string input) {
+      var prog = new Prog();
+
+      var cycle = 1;
+
+      foreach (var line in input.ToLines()) {
+        prog._instructions.Add((line, cycle));
+
+        cycle += Prog.CycleLengths[line[..4]];
+      }
+
+      return prog;
+    }
+
+    private static int GetValue(string instr) => instr.StartsWith("addx") ? int.Parse(instr.Split(" ")[1]) : 0;
+  }
+
   private readonly int[] _specialCycles = new int[] { 20, 60, 100, 140, 180, 220 };
   private const int _width = 40;
+  private const int _height = 6;
 
   public void PartOne(string input) {
-    var prog = GetProg(input);
+    var prog = Prog.GenerateFromInput(input);
 
     var values = _specialCycles
-      .Select(x => (Cycle: x, Value: GetRegisterValue(prog, x)))
+      .Select(x => (Cycle: x, Value: prog.RunFor(x)))
       .ToList();
 
     Console.WriteLine(string.Join("\n", values.Select(x => $"{x.Cycle} * {x.Value} = {x.Value * x.Cycle}")));
@@ -24,59 +67,19 @@ public class Day10 : IDay {
   }
 
   public void PartTwo(string input) {
-    var prog = GetProg(input).OrderByDescending(x => x.StartCycle).ToList();
-    var screen = Enumerable.Repeat(false, 240).ToList();
-    var regX = 1;
+    var prog = Prog.GenerateFromInput(input);
+    var crt = new CRT(_width, _height);
 
-    for (var c = 1; c <= 240; c++) {
+    for (var c = 1; c <= _width * _height; c++) {
       // Start of cycle
-      var current = prog.First(x => x.StartCycle <= c);
-      var diff = c - current.StartCycle;
-      var instrLength = _cycleLengths[current.Instr[..4]];
+      var regX = prog.RunFor(c);
 
       // During cycle
-      var pos = c - 1;
-      var spriteX = regX + ((int)(pos / _width) * _width);
-      screen[pos] = spriteX - 1 <= pos && pos <= spriteX + 1;
-      
-      // End of cycle
-      if (current.Instr.StartsWith("addx") && diff == instrLength - 1) {
-        regX += int.Parse(current.Instr.Split(" ")[1]);
-      }
+      var p = c - 1;
+      var sprX = regX + ((int)(p / _width) * _width);
+      crt.Set(p, sprX - 1 <= p && p <= sprX + 1);
     }
 
-    Console.WriteLine(GetScreenString(screen));
-  }
-
-  private List<(string Instr, int StartCycle)> GetProg(string input) {
-    var prog = new List<(string Instr, int StartCycle)>();
-    var cycle = 1;
-
-    foreach (var line in input.ToLines()) {
-      prog.Add((Instr: line, StartCycle: cycle));
-
-      cycle += _cycleLengths[line[..4]];
-    }
-
-    return prog;
-  }
-
-  private static int GetRegisterValue(List<(string Instr, int StartCycle)> prog, int cycle) {
-    var relevant = prog
-      .Where(x => x.StartCycle <= cycle - 2 && x.Instr.StartsWith("addx"));
-
-    return relevant.Aggregate(1, (acc, x) => acc + int.Parse(x.Instr.Split(" ")[1]));
-  }
-
-  private string GetScreenString(List<bool> screen) {
-    var sb = new StringBuilder();
-
-    for (var x = 0; x < screen.Count; x++) {
-      sb.Append(screen[x] ? "#" : ".");
-
-      if (x % _width == _width - 1) sb.Append("\n");
-    }
-
-    return sb.ToString();
+    Console.WriteLine(crt);
   }
 }
